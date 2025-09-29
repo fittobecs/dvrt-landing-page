044c00d96c109e64cc57adb6e3b89a9e34d70180
김종완 <fittobe.cs@gmail.com>
2025-09-29 16:52:06 +0900

    hero 모바일 화면 맞춤 수정

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero: React.FC = () => {
   return (
     <section
       id="hero"
-      className="relative flex items-center justify-center pb-0 h-[800px] pt-[85px]"
+      className="relative flex items-center justify-center pb-0 md:h-[800px] h-[420px] pt-[60px] md:pt-[85px]"
     >
       <div className="relative w-full h-full">
         {/* 이미지를 감쌀 컨테이너 */}
